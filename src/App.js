09311f59aa7b15494fe6e00d4752fa2815a3c699
Samvel Nikogosyan <nikosamvel1984@outlook.com>
2022-05-04 13:02:22 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
+// fetch https://jsonplaceholder.typicode.com/posts
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>THE APP.................</>
 }
 
-export default App;
+export default App
